fec01999d3e5b35108c8bb70b46cb86eb3c3d678
Nate Vack <njvack@wisc.edu>
2010-10-07 16:54:57 -0500

    Initial commit

---
+++ src/biopac/writers.py
@@ -0,0 +1,8 @@
+# coding: utf8
+# Part of the bioread package for reading BIOPAC data.
+#
+# Copyright (c) 2010 Board of Regents of the University of Wisconsin System
+#
+# Written by John Ollinger <ollinger@wisc.edu> and Nate Vack <njvack@wisc.edu>
+# at the Waisman Laboratory for Brain Imaging and Behavior, University of
+# Wisconsin-Madison
