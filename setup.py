86371a2c9370576325fcfbd7c79fe74e3b373c69
Nate Vack <njvack@wisc.edu>
2010-10-11 14:51:22 -0500

    Skeletal setup.py file

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf8
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name = "bioread",
+    version = "0.1",
+    package_dir = {'':'src'},
+    packages = find_packages('src'),
+    install_requires = [
+        "numpy",
+    ],
+    
+    entry_points = {
+        'console_scripts': [
+        ]
+    },
+
+    package_data = {
+    },
+
+    # metadata for upload to PyPI
+    author = "John Ollinger",
+    author_email = "ollinger@wisc.edu",
+    description = ("Utilities to read BIOPAC AcqKnowledge files"),
+    license = "GPL 2.0",
+    keywords = "science research",
+    url = "http://github.com/njvack/bioread",
+    classifiers = (
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Information Analysis"
+    ),
+)
