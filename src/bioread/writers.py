0071b71bac8b552526c372885966696bf10bd222
Nate Vack <njvack@wisc.edu>
2010-10-22 12:28:11 -0500

    Implement matlab writing

--- src/bioread/writers.py
+++ src/bioread/writers.py
@@ -6,3 +6,56 @@
 # Written by John Ollinger <ollinger@wisc.edu> and Nate Vack <njvack@wisc.edu>
 # at the Waisman Laboratory for Brain Imaging and Behavior, University of
 # Wisconsin-Madison
+
+from scipy.io import savemat
+
+class MatlabWriter(object):
+    def __init__(self, 
+        data=None, filename=None, format='5', compress=False, oned_as='row'):
+        self.data = data
+        self.filename = filename
+        self.format = format
+        self.compress = compress
+        self.oned_as = oned_as
+        
+    
+    @classmethod
+    def write_file(cls, data, filename, format='5', compress=False, 
+        oned_as='row'):
+        writer = cls(data, filename, format, compress, oned_as)
+        writer.write()
+    
+    def write(self):
+        d = self.__build_dict(self.data)
+        savemat(self.filename, d, 
+            format=self.format, do_compression=self.compress, 
+            oned_as=self.oned_as)
+        
+    
+    def __build_dict(self, data):
+        d = {}
+        d['samples_per_second'] = data.samples_per_second
+        channels = {}
+        channel_headers = {}
+        channel_dtype_headers = {}
+        for i in range(len(data.channels)):
+            c = data.channels[i]
+            label = "n%s" % (i+1)
+            chan_dict = {}
+            chan_dict['data'] = c.data
+            chan_dict['samples_per_second'] = c.samples_per_second
+            chan_dict['name'] = c.name
+            channels[label] = chan_dict
+            channel_headers[label] = data.channel_headers[i].data
+            channel_dtype_headers[label] = data.channel_dtype_headers[i].data
+            
+        d['channels'] = channels
+        
+        d['headers'] = {
+             'graph'         : data.graph_header.data
+            ,'foreign'       : data.foreign_header.data
+            ,'channel'       : channel_headers
+            ,'channel_dtype' : channel_dtype_headers
+        }
+        
+        return d
