6a89a337e02338f6cadc78af95606882e8102ca7
Nate Vack <njvack@zot.(none)>
2011-01-12 15:31:56 -0600

    Bump setup version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "bioread",
-    version = "0.8.3",
+    version = "0.8.4",
     package_dir = {'':'src'},
     packages = find_packages('src'),
     install_requires = [
