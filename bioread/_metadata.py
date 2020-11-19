b4a2d693cc3884cb1c31448cb4dc0936bbef1afd
Nate Vack <njvack@wisc.edu>
2020-11-19 14:00:35 -0600

    Bump version

--- bioread/_metadata.py
+++ bioread/_metadata.py
@@ -10,7 +10,7 @@
 
 # NOTE: This file must not import anything, or it will break installation.
 
-version_tuple = (2, 0, 0)
+version_tuple = (2, 1, 0)
 version = ".".join([str(p) for p in version_tuple])
 version_description = "bioread {0}".format(version)
 
